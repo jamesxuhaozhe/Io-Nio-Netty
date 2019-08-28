2d10bd1d1cf61af3eb5e6a8d29e5fb071d9f6fdd
Xu Haozhe <xuhaozhe@ibike668.com>
2019-08-28 22:49:02 +0800

    first commit

---
+++ src/main/java/io/IOClient.java
@@ -0,0 +1,27 @@
+package io;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+public class IOClient {
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("localhost", 8000);
+                while (true) {
+                    try {
+                        socket.getOutputStream().write((new Date() + ": hello world").getBytes());
+                        Thread.sleep(2000);
+                    } catch (Exception e) {
+
+                    }
+                }
+            } catch (IOException e) {
+
+            }
+        }).start();
+    }
+
+}
